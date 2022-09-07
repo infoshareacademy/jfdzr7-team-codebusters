3d2fd7c60d0cd55b5aad4831a14d91a96986093f
Anna Mirocha <annamirocha93@gmail.com>
2022-09-07 20:14:13 +0200

    add basic price range selection

--- src/componts/pages/bookstore/BookstorePage.js
+++ src/componts/pages/bookstore/BookstorePage.js
@@ -6,9 +6,28 @@ export const BookstorePage = () => {
     const sortOptions = ['autor', 'title', 'price']
     const [checkedCategoryOfBook, setCheckedCategoryOfBook] = useState('all')
     const [selectedSortOption, setSelectedSortOption] = useState('title')
+    const [selectedPriceRange, setSelectedPriceRange] = useState({
+        minPrice: 0,
+        maxPrice: 100
+    })
     const handleOptionChange = (event) => {
         setSelectedSortOption(event.currentTarget.value)
     }
+    const handlePriceChange = (event, partOfPrice) => {
+        if (partOfPrice === 'minPrice' &&
+            +event.currentTarget.value > selectedPriceRange.maxPrice) {
+            event.currentTarget.value = selectedPriceRange.maxPrice
+        }
+        if (partOfPrice === 'maxPrice' &&
+            +event.currentTarget.value < selectedPriceRange.minPrice) {
+            event.currentTarget.value = selectedPriceRange.minPrice
+        }
+        setSelectedPriceRange({
+            ...selectedPriceRange,
+            [partOfPrice]: +event.currentTarget.value
+        })
+    }
+    console.log(selectedPriceRange)
     return (
         <main>
             <form className="CategoryButtonsContainer">
@@ -24,6 +43,21 @@ export const BookstorePage = () => {
                             onChange={event => handleOptionChange(event)}>
                             {sortOptions.map(option => <option key={option} value={option}> {option} </option>)}
                         </select>
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="priceRangeInput">
+                        Price:
+                        <input type='number'
+                            min={0}
+                            max={selectedPriceRange.maxPrice}
+                            value={selectedPriceRange.minPrice}
+                            onChange={event => handlePriceChange(event, 'minPrice')} />-
+                        <input type='number'
+                            min={selectedPriceRange.minPrice}
+                            max={100}
+                            value={selectedPriceRange.maxPrice}
+                            onChange={event => handlePriceChange(event, 'maxPrice')} />
                     </label>
                 </div>
             </form>
