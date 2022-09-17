c92368d660b9bf60b095321f181e58c646fc3f07
bkulpa <bartek.kulpa95@gmail.com>
2022-09-17 09:59:10 +0200

    Added API for sending email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
-import { StyledButton, StyledDiv, StyledH2 } from "./Contact.styled";
-import { InputField } from "./InputField";
-import { TextareaField } from "./TextareaField";
+import { useState, useRef } from 'react';
+import { StyledButton, StyledDiv, StyledH2 } from './Contact.styled';
+import { InputField } from './InputField';
+import { TextareaField } from './TextareaField';
+import emailjs from '@emailjs/browser';
+
+
 
 export const Contact = () => {
+    const form = useRef();
+
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(`Name: ${name}\nEmail: ${email}\nMessage:\n${message}`);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        e.preventDefault();
+        emailjs.sendForm(
+            'service_63ullf7',
+            'template_6u0hm8a',
+            form.current,
+            'ieDQU1btk6_-lNbJd')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        alert('Message has been sent!');
         setEmail('');
         setMessage('');
         setName('');
@@ -19,11 +36,11 @@ export const Contact = () => {
     return (
         <StyledDiv>
             <StyledH2>If you have any questuion<br />send the message!</StyledH2>
-            <form onSubmit={handleSubmit}>
-                <InputField title={'Name'} type={'text'} value={name} setValue={setName} />
-                <InputField title={'Email'} type={'email'} value={email} setValue={setEmail} />
-                <TextareaField title={'Message'} value={message} setValue={setMessage} />
-                <StyledButton type="submit" value='Send' />
+            <form onSubmit={handleSubmit} ref={form}>
+                <InputField title={'Name'} type={'text'} value={name} setValue={setName} name={'user_name'} />
+                <InputField title={'Email'} type={'email'} value={email} setValue={setEmail} name={'user_email'} />
+                <TextareaField title={'Message'} value={message} setValue={setMessage} name={'message'} />
+                <StyledButton type='submit' value='Send' />
             </form>
         </StyledDiv >
     );
--- src/components/contact/TextareaField.jsx
+++ src/components/contact/TextareaField.jsx
@@ -1,11 +1,12 @@
 import { StyledTextarea } from "./Contact.styled";
 
-export const TextareaField = ({ title, value, setValue }) => {
+export const TextareaField = ({ title, value, setValue, name }) => {
     return (
         <>
             <label>{title}<br />
                 <StyledTextarea
                     value={value}
+                    name={name}
                     onChange={(e) => { setValue(e.target.value) }}
                 />
             </label><br />
