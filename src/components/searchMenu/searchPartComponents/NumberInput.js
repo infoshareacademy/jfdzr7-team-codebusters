a65ac5c5099497b61afbc68c7996453e35214128
Anna Mirocha <annamirocha93@gmail.com>
2022-09-20 19:31:35 +0200

    create NumberInput

---
+++ src/components/searchMenu/searchPartComponents/NumberInput.js
@@ -0,0 +1,52 @@
+import { useContext } from 'react'
+import { BooksSearchContext } from '../../../context/BooksSearchContext'
+
+export const NumberInput = ({ name, labelText, value, partOfPrice }) => {
+    const { searchConditions, setSearchConditions } = useContext(BooksSearchContext)
+    const selectedPriceRange = searchConditions.selectedPriceRange
+    const checkPriceChange = (currentValue, partOfPrice) => {
+        if (partOfPrice === 'minPrice' &&
+            +currentValue > selectedPriceRange.maxPrice) {
+            currentValue = selectedPriceRange.maxPrice
+            setSearchConditions({
+                ...searchConditions,
+                selectedPriceRange: {
+                    ...selectedPriceRange,
+                    [partOfPrice]: +currentValue
+                }
+            })
+        }
+        if (partOfPrice === 'maxPrice' &&
+            +currentValue < selectedPriceRange.minPrice) {
+            currentValue = selectedPriceRange.minPrice
+            setSearchConditions({
+                ...searchConditions,
+                selectedPriceRange: {
+                    ...selectedPriceRange,
+                    [partOfPrice]: +currentValue
+                }
+            })
+        }
+    }
+    const handlePriceChange = (currentValue, partOfPrice) => {
+        setSearchConditions({
+            ...searchConditions,
+            selectedPriceRange: {
+                ...selectedPriceRange,
+                [partOfPrice]: +currentValue
+            }
+        })
+    }
+    return (
+        <label htmlFor={name}>{labelText}
+            <input
+                name={name}
+                type='text'
+                value={value}
+                pattern="\d*"
+                onChange={event => handlePriceChange(event.currentTarget.value, partOfPrice)}
+                onBlur={event => checkPriceChange(event.currentTarget.value, partOfPrice)}
+            />
+        </label>
+    )
+}
