6efd4ca93da66fac6baa67221946f7f0b0db4add
Anna Mirocha <annamirocha93@gmail.com>
2022-09-22 17:39:04 +0200

    correct class to className

--- src/components/navigation/AccountMenuComponents/MenuPanel.js
+++ src/components/navigation/AccountMenuComponents/MenuPanel.js
@@ -2,12 +2,12 @@ import { useContext } from "react"
 import { AuthContext } from "../../../providers/AuthProvider"
 import { StyledAccountManagmentPanel } from "./StyledAccountManagmentPanel"
 import { StyledLinksPanel } from "./StyledLinksPanel"
-import { userAccountMenuLinks, adminAccountMenuLinks } from "./utils/accauntMenuLinksList"
+import { userAccountMenuLinks, adminAccountMenuLinks } from "../utils/accauntMenuLinksList"
 
 export const MenuPanel = ({ className }) => {
     const { isAdmin } = useContext(AuthContext)
     return (
-        <div class={className}>
+        <div className={className}>
             <StyledAccountManagmentPanel />
             <StyledLinksPanel linksList={isAdmin ? adminAccountMenuLinks : userAccountMenuLinks} />
         </div>
