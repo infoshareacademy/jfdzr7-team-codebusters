814245e1cbc4d9c2a28ef58da3a0c5347b68a0e3
Anna Mirocha <annamirocha93@gmail.com>
2022-09-26 23:11:04 +0200

    import changeEditStatus function instead local function

--- src/components/ordersTable/tableParts/StatusInfoPanel.js
+++ src/components/ordersTable/tableParts/StatusInfoPanel.js
@@ -1,15 +1,12 @@
 import pencil from "./../../../img/icons/pencil.png"
+import { changeEditStatus } from "./statusChangeHandlers"
 import { Wrapper } from "./Wrapper"
 
 export const StatusInfoPanel = ({ status, isEditStatusActive, setIsEditStatusActive }) => {
-    const changeEditStatus = (event) => {
-        setIsEditStatusActive(!isEditStatusActive)
-        event.stopPropagation()
-    }
     return (
         <Wrapper>
             <p>{status}</p>
-            <img src={pencil} height="20px" alt="edit" onClick={event => changeEditStatus(event)} />
+            <img src={pencil} height="20px" alt="edit" onClick={event => changeEditStatus(event, isEditStatusActive, setIsEditStatusActive)} />
         </Wrapper>
     )
 }
