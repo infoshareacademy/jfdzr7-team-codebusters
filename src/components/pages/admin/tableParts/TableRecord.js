7ac0103b66192f4dc656b5d2c22595fffa95d84c
Anna Mirocha <annamirocha93@gmail.com>
2022-09-26 22:38:07 +0200

    move expand or hide order details function into separate file

--- src/components/pages/admin/tableParts/TableRecord.js
+++ src/components/pages/admin/tableParts/TableRecord.js
@@ -4,15 +4,10 @@ import { getUsername } from './../../../../utils/getUsername'
 import { dateToString } from "../../../../utils/dateToString"
 import { StatusChangePanel } from "./StatusChangePanel"
 import { StatusInfoPanel } from "./StatusInfoPanel"
+import { expandOrHideOrderDetails } from "./expandOrHideOrderDetails"
 
 export const TableRecord = ({ className, order, index }) => {
-    const handleClick = (event) => {
-        const detailPanel = event.currentTarget.lastChild
-        const detailPanelIsHidden = detailPanel.style.display === 'none'
-        detailPanel.style.display = detailPanelIsHidden ? 'block' : 'none'
-    }
     const [isEditStatusActive, setIsEditStatusActive] = useState(false)
-
     const [username, setUsername] = useState('')
 
     useEffect(() => {
@@ -20,15 +15,23 @@ export const TableRecord = ({ className, order, index }) => {
     }, [order.userID])
 
     return (
-        <div className={className} onClick={event => handleClick(event)} >
+        <div className={className} onClick={event => expandOrHideOrderDetails(event)} >
             <p>{index + 1}</p>
             <p>{dateToString(order.orderDate)}</p>
             <p>{username}</p>
             <p>{order.orderValue}</p>
             {
                 isEditStatusActive ?
-                    <StatusChangePanel order={order} isEditStatusActive={isEditStatusActive} setIsEditStatusActive={setIsEditStatusActive} /> :
-                    <StatusInfoPanel status={order.status} isEditStatusActive={isEditStatusActive} setIsEditStatusActive={setIsEditStatusActive} />
+                    <StatusChangePanel
+                        order={order}
+                        isEditStatusActive={isEditStatusActive}
+                        setIsEditStatusActive={setIsEditStatusActive}
+                    /> :
+                    <StatusInfoPanel
+                        status={order.status}
+                        isEditStatusActive={isEditStatusActive}
+                        setIsEditStatusActive={setIsEditStatusActive}
+                    />
             }
             <StyledOrderDetail positions={order.positions} />
         </div >
