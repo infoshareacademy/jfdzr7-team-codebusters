8cf1f9cfa8e0aefa777ae5841d13649dc837ce1e
Anna Mirocha <annamirocha93@gmail.com>
2022-09-27 00:05:50 +0200

    add status condition to search

--- src/utils/getOrderList.js
+++ src/utils/getOrderList.js
@@ -3,11 +3,19 @@ import { db } from '../api/firebase'
 
 export const getOrderList = ({ setOrderList, checkedOption, orderDate, searchText }) => {
     const collectionRef = collection(db, 'orders')
-    getDocs(query(collectionRef,
-        where('orderDate', ">=", orderDate.dateStart),
-        where('orderDate', "<=", orderDate.dateEnd),
-        orderBy('orderDate', 'desc')
-    )).then(querySnapshot => {
+    const searchConditions = checkedOption === 'all' ?
+        query(collectionRef,
+            where('orderDate', ">=", orderDate.dateStart),
+            where('orderDate', "<=", orderDate.dateEnd),
+            orderBy('orderDate', 'desc')
+        ) :
+        query(collectionRef,
+            where('orderDate', ">=", orderDate.dateStart),
+            where('orderDate', "<=", orderDate.dateEnd),
+            where('status', '==', checkedOption),
+            orderBy('orderDate', 'desc')
+        )
+    getDocs(searchConditions).then(querySnapshot => {
         const orderList = querySnapshot.docs.map(order => {
             return ({
                 ...order.data(),
