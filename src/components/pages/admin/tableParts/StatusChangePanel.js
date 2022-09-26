f056e0f6807afacfe06957f545dcd38e9f27c987
Anna Mirocha <annamirocha93@gmail.com>
2022-09-26 22:10:26 +0200

    status change handlers move into separate file

--- src/components/pages/admin/tableParts/StatusChangePanel.js
+++ src/components/pages/admin/tableParts/StatusChangePanel.js
@@ -3,37 +3,25 @@ import { Wrapper } from "./Wrapper"
 import cancel from "./../../../../img/icons/cancel.png"
 import check from "./../../../../img/icons/check.png"
 import { SelectInput } from "./SelectInput"
-import { doc, updateDoc } from 'firebase/firestore'
-import { db } from "../../../../api/firebase"
+import { cancelStatusChange, confirmStatusChange } from "./statusChangeHandlers"
+
 
 export const StatusChangePanel = ({ order, isEditStatusActive, setIsEditStatusActive }) => {
-    const changeEditStatus = (event) => {
-        setIsEditStatusActive(!isEditStatusActive)
-        event.stopPropagation()
-    }
-
-    const cancelStatusChange = (event) => {
-        changeEditStatus(event)
-    }
-
-    const confirmStatusChange = (order, event) => {
-        changeEditStatus(event)
-        order.status = statusInput
-        const docRef = doc(db, 'orders', order.id)
-        const newObj = { ...order, status: statusInput }
-        updateDoc(docRef, newObj)
-    }
-    const [statusInput, setStatusInput] = useState(order.status)
+    const [orderStatusSelectValue, setOrderStatusSelectValue] = useState(order.status)
     return (
         <Wrapper>
             <SelectInput labelText=''
                 name='orderStatus'
-                value={statusInput}
-                callback={event => setStatusInput(event.target.value)}
+                value={orderStatusSelectValue}
+                callback={event => setOrderStatusSelectValue(event.target.value)}
                 statusOptions={['waiting', 'active', 'sent']}
                 onClickCallback={event => event.stopPropagation()}
             />
-            <img src={check} height="20px" alt="check" onClick={event => confirmStatusChange(order, event)} />
-            <img src={cancel} height="20px" alt="cancel" onClick={event => cancelStatusChange(event)} />
+            <img src={check} height="20px"
+                alt="check"
+                onClick={event => confirmStatusChange(event, order, orderStatusSelectValue, isEditStatusActive, setIsEditStatusActive)} />
+            <img src={cancel} height="20px"
+                alt="cancel"
+                onClick={event => cancelStatusChange(event, isEditStatusActive, setIsEditStatusActive)} />
         </Wrapper>)
 }
