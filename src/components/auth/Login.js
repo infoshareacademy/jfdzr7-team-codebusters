bf2a3c27202b2ce5274604e5da5a15aebdb0fa19
Sylwia Kozon <kozon.sylwia.k@gmail.com>
2022-09-29 12:51:33 +0200

    add authHandlers

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,26 +1,11 @@
-import { signInWithEmailAndPassword } from "@firebase/auth";
-import { auth } from "../../api/firebase";
 import { AuthForm } from "./AuthForm";
-import { firebaseErrors } from "../../utils/firebaseErrors"
-import { getFormData } from "../../utils/getFormData";
+import { handleLogin } from "./authHandlers";
 
 export const Login = () => {
-  const handleLogin = (e) => {
-    e.preventDefault();
-    const { email, password } = getFormData(e);
-    signInWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        e.target.reset();
-      })
-      .catch((e) => {
-        alert(firebaseErrors[e.code]);
-      });
-  };
-
   return (
     <>
       <h2>Log in:</h2>
-      <AuthForm submitText="Log in" onSubmit={handleLogin} />
+      <AuthForm submitText="Log in" onSubmit={(e) => handleLogin(e)} />
     </>
   );
 };
