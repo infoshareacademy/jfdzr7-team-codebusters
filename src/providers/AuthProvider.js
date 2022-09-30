7f4af1f38aa5eab4fd5b6aa121c5f631aaf06cd8
Anna Mirocha <annamirocha93@gmail.com>
2022-09-30 23:04:41 +0200

    change initial value of user

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-export const AuthContext = createContext([])
+export const AuthContext = createContext(null)
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
