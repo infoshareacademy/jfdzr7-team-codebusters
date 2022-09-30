ba3b5e4e439ee4e67af0d5b306a2505ae817b338
Anna Mirocha <annamirocha93@gmail.com>
2022-09-30 13:02:37 +0200

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { Contact } from "./components/pages/contact/Contact";
 import { Footer } from "./components/footer/Footer";
-import { BookstorePage } from "./components/pages/bookstore/BookstorePage";
-import { Register } from "./components/auth/Register";
-import { ForgotPassword } from "./components/auth/ForgotPassword";
-import { Login } from "./components/auth/Login";
 
 export const App = () => {
   return (
     <div className="App">
-      {/* <Contact /> */}
-      {/* <BookstorePage /> */}
-      {/* <Register /> */}
-      {/* <ForgotPassword /> */}
-      {/* <Login /> */}
       <Footer />
     </div>
   );
