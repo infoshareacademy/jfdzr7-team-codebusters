47cc9344dfd4457601eb18a79422004935881d3b
Anna Mirocha <annamirocha93@gmail.com>
2022-09-30 16:48:51 +0200

    change style in NavLink

--- src/components/navigation/StyledNavLink.js
+++ src/components/navigation/StyledNavLink.js
@@ -4,7 +4,10 @@ import styled from "styled-components";
 export const StyledNavLink = styled(NavLink)`
     color: #220901;
     font-weight: bold;
-    font-size: 32px;
+    font-size: 28px;
     text-decoration: none;
     padding: 20px;
+    &.active {
+        color: #FF902B;
+    }
 `
