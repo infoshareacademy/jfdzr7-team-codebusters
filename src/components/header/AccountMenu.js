4c70a95582d3960939b6a37647555e69a9e52281
Sylwia Kozon <kozon.sylwia.k@gmail.com>
2022-10-07 15:15:09 +0200

    refactoring

--- src/components/header/AccountMenu.js
+++ src/components/header/AccountMenu.js
@@ -1,11 +1,26 @@
-import { AccountMenuPanel } from "./AccountMenuComponents/AccountMenuPanel"
-import { StyledAccountMenu } from "./Header.styled"
+import { AccountMenuPanel } from "./AccountMenuComponents/AccountMenuPanel";
+import { StyledAccountMenu } from "./Header.styled";
+import { AuthContext } from "../../providers/AuthProvider";
+import { CartContext } from "../../providers/CartProvider";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { findCart } from "../../utils/cartdbHandlers";
 
 export const AccountMenu = () => {
-    return (
-        <StyledAccountMenu>
-            <p>Account</p>
-            <AccountMenuPanel />
-        </StyledAccountMenu>
-    )
-}
+  const { cart, cartId, setCart, setCartId } = useContext(CartContext);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (findCart(user, setCart, setCartId) !== [])
+      findCart(user, setCart, setCartId);
+    console.log(cartId);
+    console.log(cart);
+  }, [user]);
+
+  return (
+    <StyledAccountMenu>
+      <p>Account</p>
+      <AccountMenuPanel />
+    </StyledAccountMenu>
+  );
+};
