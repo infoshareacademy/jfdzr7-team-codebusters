82e18bba06d40e6b30c9cad9d5156ef1d5733135
Anna Mirocha <annamirocha93@gmail.com>
2022-10-08 08:24:08 +0200

    correct time string format

--- src/utils/dateToString.js
+++ src/utils/dateToString.js
@@ -8,8 +8,8 @@ export const dateToString = (date) => {
 
 export const timeToString = (date) => {
     const hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
-    const minutes = date.getMinutes() ? '0' + date.getMinutes() : date.getMinutes()
-    const seconds = date.getSeconds() ? '0' + date.getSeconds() : date.getSeconds()
+    const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+    const seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
     const timeString = `${hour}:${minutes}:${seconds}`
     return timeString
 }
