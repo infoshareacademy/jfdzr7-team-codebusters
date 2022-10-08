df075d93b56828a4e1e2e975c904e3898427d13d
bkulpa <bartek.kulpa95@gmail.com>
2022-10-09 00:53:38 +0200

    Deleted NewBook component from App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { Header } from "./components/header/Header";
 import { AuthProvider } from "./providers/AuthProvider";
 import { Routing } from "./components/routing/Routing";
 import './App.css';
-import { NewBook } from "./components/pages/newBook/NewBook";
 
 export const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <AuthProvider>
-          <NewBook />
           <Header />
           <Routing />
           <Footer />
