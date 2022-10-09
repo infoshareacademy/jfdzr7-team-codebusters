c4f1d00b3a457ae6154d54c0833f20a7be39323c
Sylwia Kozon <kozon.sylwia.k@gmail.com>
2022-10-09 08:49:58 +0200

    fix AuthProvider Context and add method to refresh user data

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,13 +1,33 @@
 import { createContext, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../api/firebase";
+import { useEffect } from "react";
+import { getDoc, doc } from "firebase/firestore";
+import { getUser } from "../utils/getUser";
+import { db } from "../api/firebase";
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const isAuth = !!user;
+  const [isAuth, setIsAuth] = useState(!!user);
+  // let isAuth = !!user;
   const isAdmin = !!user?.isAdmin;
   const isUser = isAuth && !user?.isAdmin;
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (userData) => {
+      console.log(userData);
+      if (userData) {
+        getDoc(doc(db, "users", userData.uid)).then((querySnapshot) => {
+          getUser(setUser, userData.uid, setIsAuth);
+        });
+      } else {
+        setUser(null);
+        setIsAuth(false);
+      }
+    });
+  }, []);
   return (
     <AuthContext.Provider
       value={{
